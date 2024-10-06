e389f030ac26b1f8f7602bf23c3a2509dd650b04
Frances Pasulohan <franpas12123@gmail.com>
2024-10-06 18:28:18 +0800

    create AppSection component

---
+++ src/components/AppSection.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const { ariaLabel } = defineProps<{
+  ariaLabel: string;
+  title: string;
+}>();
+</script>
+
+<template>
+  <div
+    class="sticky top-0 scroll-mt-16 bg-slate-900/75 backdrop-blur"
+    :aria-label="ariaLabel"
+  >
+    <h2
+      class="text-md py-6 font-bold uppercase tracking-wide text-slate-200 lg:sr-only"
+    >
+      {{ title }}
+    </h2>
+  </div>
+  <slot>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa incidunt
+      similique vel quos, libero assumenda suscipit excepturi asperiores quas
+      provident neque quisquam pariatur laudantium ipsa impedit placeat harum?
+      Consequatur, labore!
+    </p>
+    <br />
+  </slot>
+</template>
+
+<style scoped></style>
