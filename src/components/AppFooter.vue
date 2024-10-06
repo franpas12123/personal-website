5945dc3401e561a04dffee90575b2e4708cfe06e
Frances Pasulohan <franpas12123@gmail.com>
2024-10-06 18:27:48 +0800

    create AppFooter component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts"></script>
+<template>
+  <footer class="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
+    <p>
+      Inspired by
+      <a
+        href="https://brittanychiang.com/"
+        class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+        target="_blank"
+        rel="noreferrer noopener"
+        aria-label="Brittany Chiang's website (opens in a new tab)"
+        >Brittany Chiang</a
+      >, this website is built using
+      <a
+        href="https://vuejs.org/"
+        class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+        target="_blank"
+        rel="noreferrer noopener"
+        aria-label="Vue.js (opens in a new tab)"
+        >Vue.js</a
+      >,
+      <a
+        href="https://tailwindcss.com/"
+        class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+        target="_blank"
+        rel="noreferrer noopener"
+        aria-label="Tailwind CSS (opens in a new tab)"
+        >Tailwind CSS</a
+      >, and
+      <a
+        href="https://vite.dev/"
+        class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+        target="_blank"
+        rel="noreferrer noopener"
+        aria-label="Vite.dev (opens in a new tab)"
+        >Vite</a
+      >. Developed in
+      <a
+        href="https://code.visualstudio.com/"
+        class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+        target="_blank"
+        rel="noreferrer noopener"
+        aria-label="Visual Studio Code (opens in a new tab)"
+        >Visual Studio Code</a
+      >
+      and deployed on
+      <a
+        href="https://pages.github.com/"
+        class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+        target="_blank"
+        rel="noreferrer noopener"
+        aria-label="Github Pages (opens in a new tab)"
+        >Github Pages</a
+      >. All icons are from
+      <a
+        href="https://icons8.com"
+        class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+        target="_blank"
+        rel="noreferrer noopener"
+        aria-label="Icons8 (opens in a new tab)"
+        >Icons8</a
+      >.
+    </p>
+  </footer>
+</template>
+
+<style scoped></style>
