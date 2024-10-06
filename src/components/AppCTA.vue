eaf6699b43f0748be67b23c0ba48b07b761396f5
Frances Pasulohan <franpas12123@gmail.com>
2024-10-06 18:27:30 +0800

    create AppCTA component

---
+++ src/components/AppCTA.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="flex flex-col justify-center">
+    <p class="mt-4 font-extralight">
+      I'm currently available for freelance work. If you have a project in mind,
+      I'd love to hear from you.
+    </p>
+    <a
+      class="mt-5 rounded-full bg-slate-400 bg-teal-400/10 px-4 py-4 text-center text-xl leading-5 tracking-wide text-teal-300"
+      href="mailto:francespasulohan@gmail.com"
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      Get in touch
+    </a>
+  </div>
+</template>
+
+<style scoped></style>
