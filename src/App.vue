f1a7ecad42c276ee4b7c4a71bf9d8ed4083e53eb
Frances Pasulohan <franpas12123@gmail.com>
2024-10-16 08:57:49 +0800

    added fullstory exp

--- src/App.vue
+++ src/App.vue
@@ -77,6 +77,7 @@ const { navigation } = useStore();
                 'Ionic',
                 'Braze',
                 'Storyblok',
+                'FullStory',
               ]"
             >
               <p>
